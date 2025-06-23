d0f613ae0a4d20061d72ee2243d9b8cb97b4c7bd
larbish <leproux.baptiste@gmail.com>
2025-06-23 11:13:21 +0000

    Deploying to gh-pages from @ larbish/docus-studio@7339c0c7b08eb49b74ff256f5903811a6e3f0c35 🚀

--- _nuxt/C1HPTgqa.js
+++
@@ -1,1 +0,0 @@
-import{r as s}from"./B5XL4Bfh.js";import t from"./DPkG9yU7.js";import{z as e,H as a,i as p,w as c,V as i}from"./Dthoxbbf.js";import"./C2bHYaEW.js";const d={__name:"ProseTip",setup(n){const o=e();return(r,f)=>(p(),a(t,{color:"success",icon:i(o).ui.icons.tip},{default:c(()=>[s(r.$slots,"default",{mdcUnwrap:"p"})]),_:3},8,["icon"]))}};export{d as default};
---
+++ _nuxt/CVjrOxGG.js
@@ -0,0 +1,1 @@
+import{r as n}from"./CQLj9nvK.js";import a from"./PCki21sB.js";import{z as s,H as t,i as e,w as i,V as p}from"./C97LdVSL.js";import"./C2bHYaEW.js";const d={__name:"ProseWarning",setup(c){const o=s();return(r,f)=>(e(),t(a,{color:"warning",icon:p(o).ui.icons.warning},{default:i(()=>[n(r.$slots,"default",{mdcUnwrap:"p"})]),_:3},8,["icon"]))}};export{d as default};
