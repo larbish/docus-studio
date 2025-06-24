477569fb15e3c351641e3a0157a3bf5f7792f286
larbish <leproux.baptiste@gmail.com>
2025-06-24 09:45:21 +0000

    Deploying to gh-pages from @ larbish/docus-studio@a01f412a6ac37b424ca044270090e316b4334081 🚀

--- _nuxt/B1bg_6Pl.js
+++
@@ -1,1 +0,0 @@
-import{r as s}from"./CQLj9nvK.js";import t from"./PCki21sB.js";import{z as e,H as a,i as p,w as c,V as i}from"./C97LdVSL.js";import"./C2bHYaEW.js";const d={__name:"ProseTip",setup(n){const o=e();return(r,f)=>(p(),a(t,{color:"success",icon:i(o).ui.icons.tip},{default:c(()=>[s(r.$slots,"default",{mdcUnwrap:"p"})]),_:3},8,["icon"]))}};export{d as default};
